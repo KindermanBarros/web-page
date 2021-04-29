46d99e38e5868d15cf44e611dc82e97e283f1794
KindermanBarros <root@Joy.localdomain>
2021-04-29 10:51:51 -0400

    Mudada estrutura de dados em arvore

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,378 @@
-import {useState} from 'react';
-import styles from './index.module.css';
-import fontFamily from './index.module.css';
+import "./index.module.css";
 
+function Home() {
+  return (
+    <div>
+      <title>Projeto</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="./styles.css" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Raleway"
+      />
+      <style>
+        body,h1,h2,h3,h4,h5 {((fontFamily = "Raleway"), sans - serif)}
+      </style>
 
-function Home(){
-    return (
-        <div>
-            <head>
-                <title>hewo</title>
-            </head>
-            <header className={styles.header}>Andromeda</header>
-            <div>
-                <img style={{align:'center'}} src='https://i.imgur.com/qFEoH1K.jpg?2' alt={'Clocks'}/>
-                <h1  style={{textAlign:'center', fontFamily:'Futura PT'}}>Contando</h1>
-                <Contador/>
-            </div>
-        </div>
-    )
-}
+      <body class="w3-light-grey">
+        <div class="w3-content" style="max-width:1400px">
+          <header class="w3-container w3-center w3-padding-32">
+            <h1>
+              <b>Projeto Integrador</b>
+            </h1>
+            <p>
+              Energias <span class="w3-tag">Sustentáveis</span>
+            </p>
+          </header>
 
-export default Home
-
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-    return (
-        <view style={{flex:1,justifyContent: "center",}}>
-           <div>
-             <p className={styles.bigblue}>Esse botãozinho é fofo</p>
-                <div style={{ display: 'flex', flex:1,justifyContent: "center", alignItems: "center"}}>
-                <button style={{}} variant='contained' onClick={adicionarContador}>{contador}</button>
+          <div class="w3-row">
+            <div class="w3-col l8 s12">
+              <div class="w3-card-4 w3-margin w3-white">
+                <img
+                  src="/w3images/woods.jpg"
+                  alt="Nature"
+                  style="width:100%"
+                />
+                <div class="w3-container">
+                  <h3>
+                    <b>Introdução</b>
+                  </h3>
+                  <h5>
+                    Title description,{" "}
+                    <span class="w3-opacity">April 7, 2014</span>
+                  </h5>
                 </div>
+
+                <div class="w3-container">
+                  <p>
+                    Mauris neque quam, fermentum ut nisl vitae, convallis
+                    maximus nisl. Sed mattis nunc id lorem euismod placerat.
+                    Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies mi non congue ullam corper. Praesent
+                    tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+                    porta lectus vitae, ultricies congue gravida diam non
+                    fringilla.
+                  </p>
+                  <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                      <p>
+                        <button class="w3-button w3-padding-large w3-white w3-border">
+                          <b>READ MORE »</b>
+                        </button>
+                      </p>
+                    </div>
+                    <div class="w3-col m4 w3-hide-small">
+                      <p>
+                        <span class="w3-padding-large w3-right">
+                          <b>Comments  </b> <span class="w3-tag">0</span>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr />
+
+              <div class="w3-card-4 w3-margin w3-white">
+                <img
+                  src="/w3images/bridge.jpg"
+                  alt="Norway"
+                  style="width:100%"
+                />
+                <div class="w3-container">
+                  <h3>
+                    <b>BLOG ENTRY</b>
+                  </h3>
+                  <h5>
+                    Title description,{" "}
+                    <span class="w3-opacity">April 2, 2014</span>
+                  </h5>
+                </div>
+
+                <div class="w3-container">
+                  <p>
+                    Mauris neque quam, fermentum ut nisl vitae, convallis
+                    maximus nisl. Sed mattis nunc id lorem euismod placerat.
+                    Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies mi non congue ullam corper. Praesent
+                    tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+                    porta lectus vitae, ultricies congue gravida diam non
+                    fringilla.
+                  </p>
+                  <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                      <p>
+                        <button class="w3-button w3-padding-large w3-white w3-border">
+                          <b>READ MORE »</b>
+                        </button>
+                      </p>
+                    </div>
+                    <div class="w3-col m4 w3-hide-small">
+                      <p>
+                        <span class="w3-padding-large w3-right">
+                          <b>Comments  </b> <span class="w3-badge">2</span>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="w3-card-4 w3-margin w3-white">
+                <img
+                  src="/w3images/bridge.jpg"
+                  alt="Norway"
+                  style="width:100%"
+                />
+                <div class="w3-container">
+                  <h3>
+                    <b>BLOG ENTRY</b>
+                  </h3>
+                  <h5>
+                    Title description,{" "}
+                    <span class="w3-opacity">April 2, 2014</span>
+                  </h5>
+                </div>
+
+                <div class="w3-container">
+                  <p>
+                    Mauris neque quam, fermentum ut nisl vitae, convallis
+                    maximus nisl. Sed mattis nunc id lorem euismod placerat.
+                    Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies mi non congue ullam corper. Praesent
+                    tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+                    porta lectus vitae, ultricies congue gravida diam non
+                    fringilla.
+                  </p>
+                  <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                      <p>
+                        <button class="w3-button w3-padding-large w3-white w3-border">
+                          <b>READ MORE »</b>
+                        </button>
+                      </p>
+                    </div>
+                    <div class="w3-col m4 w3-hide-small">
+                      <p>
+                        <span class="w3-padding-large w3-right">
+                          <b>Comments  </b> <span class="w3-badge">2</span>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="w3-card-4 w3-margin w3-white">
+                <img
+                  src="/w3images/bridge.jpg"
+                  alt="Norway"
+                  style="width:100%"
+                />
+                <div class="w3-container">
+                  <h3>
+                    <b>BLOG ENTRY</b>
+                  </h3>
+                  <h5>
+                    Title description,{" "}
+                    <span class="w3-opacity">April 2, 2014</span>
+                  </h5>
+                </div>
+
+                <div class="w3-container">
+                  <p>
+                    Mauris neque quam, fermentum ut nisl vitae, convallis
+                    maximus nisl. Sed mattis nunc id lorem euismod placerat.
+                    Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies mi non congue ullam corper. Praesent
+                    tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+                    porta lectus vitae, ultricies congue gravida diam non
+                    fringilla.
+                  </p>
+                  <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                      <p>
+                        <button class="w3-button w3-padding-large w3-white w3-border">
+                          <b>READ MORE »</b>
+                        </button>
+                      </p>
+                    </div>
+                    <div class="w3-col m4 w3-hide-small">
+                      <p>
+                        <span class="w3-padding-large w3-right">
+                          <b>Comments  </b> <span class="w3-badge">2</span>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="w3-card-4 w3-margin w3-white">
+                <img
+                  src="/w3images/bridge.jpg"
+                  alt="Norway"
+                  style="width:100%"
+                />
+                <div class="w3-container">
+                  <h3>
+                    <b>BLOG ENTRY</b>
+                  </h3>
+                  <h5>
+                    Title description,{" "}
+                    <span class="w3-opacity">April 2, 2014</span>
+                  </h5>
+                </div>
+
+                <div class="w3-container">
+                  <p>
+                    Mauris neque quam, fermentum ut nisl vitae, convallis
+                    maximus nisl. Sed mattis nunc id lorem euismod placerat.
+                    Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies mi non congue ullam corper. Praesent
+                    tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+                    porta lectus vitae, ultricies congue gravida diam non
+                    fringilla.
+                  </p>
+                  <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                      <p>
+                        <button class="w3-button w3-padding-large w3-white w3-border">
+                          <b>READ MORE »</b>
+                        </button>
+                      </p>
+                    </div>
+                    <div class="w3-col m4 w3-hide-small">
+                      <p>
+                        <span class="w3-padding-large w3-right">
+                          <b>Comments  </b> <span class="w3-badge">2</span>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="w3-card-4 w3-margin w3-white">
+                <img
+                  src="/w3images/bridge.jpg"
+                  alt="Norway"
+                  style="width:100%"
+                />
+                <div class="w3-container">
+                  <h3>
+                    <b>BLOG ENTRY</b>
+                  </h3>
+                  <h5>
+                    Title description,{" "}
+                    <span class="w3-opacity">April 2, 2014</span>
+                  </h5>
+                </div>
+
+                <div class="w3-container">
+                  <p>
+                    Mauris neque quam, fermentum ut nisl vitae, convallis
+                    maximus nisl. Sed mattis nunc id lorem euismod placerat.
+                    Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies mi non congue ullam corper. Praesent
+                    tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+                    porta lectus vitae, ultricies congue gravida diam non
+                    fringilla.
+                  </p>
+                  <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                      <p>
+                        <button class="w3-button w3-padding-large w3-white w3-border">
+                          <b>READ MORE »</b>
+                        </button>
+                      </p>
+                    </div>
+                    <div class="w3-col m4 w3-hide-small">
+                      <p>
+                        <span class="w3-padding-large w3-right">
+                          <b>Comments  </b> <span class="w3-badge">2</span>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </view>
-    );
+
+            <div class="w3-col l4">
+              <div class="w3-card w3-margin w3-margin-top">
+                <img src="/w3images/avatar_g.jpg" style="width:100%" />
+                <div class="w3-container w3-white">
+                  <h4>
+                    <b>My Name</b>
+                  </h4>
+                  <p>
+                    Just me, myself and I, exploring the universe of uknownment.
+                    I have a heart of love and a interest of lorem ipsum and
+                    mauris neque quam blog. I want to share my world with you.
+                  </p>
+                </div>
+              </div>
+              <hr />
+
+              <div class="w3-card w3-margin">
+                <div class="w3-container w3-padding">
+                  <h4>Tópicos Importantes</h4>
+                </div>
+                <ul class="w3-ul w3-hoverable w3-white">
+                  <li class="w3-padding-16">
+                    <img
+                      src="/w3images/workshop.jpg"
+                      alt="Image"
+                      class="w3-left w3-margin-right"
+                      style="width:50px"
+                    />
+                    <span class="w3-large">Lorem</span>
+                    <br />
+                    <span>Sed mattis nunc</span>
+                  </li>
+                  <li class="w3-padding-16">
+                    <img
+                      src="/w3images/gondol.jpg"
+                      alt="Image"
+                      class="w3-left w3-margin-right"
+                      style="width:50px"
+                    />
+                    <span class="w3-large">Ipsum</span>
+                    <br />
+                    <span>Praes tinci sed</span>
+                  </li>
+                  <li class="w3-padding-16">
+                    <img
+                      src="/w3images/skies.jpg"
+                      alt="Image"
+                      class="w3-left w3-margin-right"
+                      style="width:50px"
+                    />
+                    <span class="w3-large">Dorum</span>
+                    <br />
+                    <span>Ultricies congue</span>
+                  </li>
+                  <li class="w3-padding-16 w3-hide-medium w3-hide-small">
+                    <img
+                      src="/w3images/rock.jpg"
+                      alt="Image"
+                      class="w3-left w3-margin-right"
+                      style="width:50px"
+                    />
+                    <span class="w3-large">Mingsum</span>
+                    <br />
+                    <span>Lorem ipsum dipsum</span>
+                  </li>
+                </ul>
+              </div>
+              <hr />
+            </div>
+          </div>
+          <br />
+        </div>
+      </body>
+    </div>
+  );
 }
